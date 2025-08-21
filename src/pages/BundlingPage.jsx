80f60efd776043b1969e1359fee4abacdd716e65
Push4ck <pushkarsharma652@gmail.com>
2025-08-21 14:06:23 +0530

    one image added.

--- src/pages/BundlingPage.jsx
+++ src/pages/BundlingPage.jsx
@@ -12,7 +12,8 @@ const mockProduct = {
   salePrice: 4566,
   sku: "blk/m/ins0123",
   barcode: "4560000000000",
-  image: "https://via.placeholder.com/150",
+  image:
+    "https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 };
 
 // Reusable Button
